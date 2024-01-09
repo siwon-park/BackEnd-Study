0e0222c8037f205152fa43f44ae3ac47edf3f6f5
siwon-park <zow777@naver.com>
2024-01-10 00:04:29 +0900

    docs: 토비의 스프링부트 학습 3일차

--- Spring_Study/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring_Study/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,13 @@
 package tobyspring.helloboot;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
@@ -11,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody // 디스패처 서블릿이 String 결과에 해당하는 이름의 뷰를 찾기 때문에 값 자체를 반환하기 위함
     public String hello(String name) { // 쿼리 스트링으로 동작 /hello?name=
 //        SimpleHelloService helloService = new SimpleHelloService();
         return helloService.sayHello(Objects.requireNonNull(name));
--- Spring_Study/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring_Study/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,10 +2,13 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +20,21 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 생성 -> 어플리케이션 구성 정보
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				// 디스패처 서블릿
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this) // GenericWebApplicationContext 클래스를 호출해야만 사용 가능
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class); // 클래스 정보를 Bean에 담아 등록함
 		/*
 		* 그런데 스프링 컨테이너는 어떻게 HelloController가 SimpleHelloService를 사용할 것인지 알 수 있는가?
@@ -27,14 +44,14 @@ public class HellobootApplication {
 		* 의존성을 주입한다.
 		* */
 		applicationContext.registerBean(SimpleHelloService.class); // 명확하게 어떤 클래스인지 등록해줘야 함
-		applicationContext.refresh(); // 빈을 생성(초기화)
+		applicationContext.refresh(); // 빈을 생성(초기화) -> 스프링 컨테이너를 초기화
 
 		// 톰캣 서버 시작 -> 그러나 실제 톰캣을 설치해서 보면 많은 설정이 필요한 것처럼
 		// .start()만으로 실행하기는 준비해야할 것이 많아서 쉽지 않다.
 //		new Tomcat().start();
 
 		// TomcatServletWebServerFactory; 스프링부트가 톰캣 서블릿 컨테이너를 쉽게 띄울 수 있도록 만든 클래스
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 //		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
 //			@Override
 //			public void onStartup(ServletContext servletContext) throws ServletException {
@@ -59,29 +76,29 @@ public class HellobootApplication {
 		*/
 		
 		// 프론트 컨트롤러
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-//			HelloController helloController = new HelloController(); // HelloController 생성
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
+//		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+////			HelloController helloController = new HelloController(); // HelloController 생성
+//			servletContext.addServlet("frontcontroller", new HttpServlet() {
+//				@Override
+//				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+//						String name = req.getParameter("name");
+//
+//						HelloController helloController = applicationContext.getBean(HelloController.class); // Bean으로 받음
+//						String ret = helloController.hello(name);
+//						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+//						resp.getWriter().println(ret);
+//					} else if (req.getRequestURI().equals("/user")) {
+//						//
+//					} else {
+//						resp.setStatus(HttpStatus.NOT_FOUND.value()); // 404반환
+//					}
+//				}
+//			}).addMapping("/*"); // "/hello"로 들어오는 요청을 HttpServlet 익명 클래스에서 처리하겠다는 의미
+//
+//		});
 
-						HelloController helloController = applicationContext.getBean(HelloController.class); // Bean으로 받음
-						String ret = helloController.hello(name);
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					} else if (req.getRequestURI().equals("/user")) {
-						//
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value()); // 404반환
-					}
-				}
-			}).addMapping("/*"); // "/hello"로 들어오는 요청을 HttpServlet 익명 클래스에서 처리하겠다는 의미
 
-		});
-
-		webServer.start();
 	}
 
 }
