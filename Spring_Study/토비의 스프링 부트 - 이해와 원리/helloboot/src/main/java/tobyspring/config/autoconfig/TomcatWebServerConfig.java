d5286350b08a6ab51c8b338a5d7af1cc21481c27
siwon-park <zow777@naver.com>
2024-02-04 15:00:43 +0900

    docs: 토비의 스프링부트 학습 7일차(2)

--- Spring_Study/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ Spring_Study/토비의 스프링 부트 - 이해와 원리/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
@@ -12,6 +13,14 @@ import tobyspring.config.ConditionalMyOnClass;
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
     @Bean("TomcatWebServerFactory")
+    @ConditionalOnMissingBean // 해당 타입으로 스프링 빈이 등록된 것이 없으면 등록함 -> 사용자가 직접 작성하여 등록한게 있으면 직접 작성한 것을 우선시 하기 위함
+    /*
+    * 단, ConditionalOnMissingBean을 적용할 때는 Configuration 클래스의 적용 순서가 중요하다.
+    * 컨테이너에 등록된 빈 정보를 기준으로 체크하기 때문에 그렇다.
+    * 개발자가 직접 정의한 커스텀 빈 구성 정보는 자동 구성 정보보다 우선 처리되지만,
+    * 자동 구성 정보에서 이를 사용할 경우에는 나중에 등록될 빈에 포함되어 등록될 예정인데
+    * Configuration 순서가 더 앞서서 먼저 등록되는 일이 발생하여 빈 오브젝트 등록에 문제가 생긴다.
+    * */
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
